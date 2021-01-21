81493fa66875c40b6a98884c8888b01f76dd8885
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2021-01-21 23:06:31 +0100

    Fix: Guessing game tweaks

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,22 +4,33 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number");
-    println!("Input your guess");
+    
 
     let secret_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!("Input your guess");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
-
-    stdin().read_line(&mut guess).expect("Failed line to read");
+        stdin().read_line(&mut guess).expect("Failed line to read");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input, I need a number");
+                continue;
+            },
+        };
     
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 
 
