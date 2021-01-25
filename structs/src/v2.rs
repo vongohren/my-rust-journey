d5fc1a4a2b32a9328d7a7be5788ac3bcd69cd9ed
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2021-01-25 22:48:53 +0100

    more structs and things

--- structs/src/v2.rs
+++ structs/src/v2.rs
@@ -8,6 +8,16 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 pub fn main() {
@@ -16,9 +26,33 @@ pub fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 20,
+        height: 10,
+    };
+
+    let rect3 = Rectangle {
+        width: 50,
+        height: 60,
+    };
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
+    );
+
+    println!(
+        "Can hold the rect2: {}",
+        rect1.can_hold(&rect2)
+    );
+    println!(
+        "Can hold the rect3: {}",
+        rect1.can_hold(&rect3)
+    );
+
+    println!(
+        "A square rect: {:#?}",
+        Rectangle::square(32)
     );
 }    
 
