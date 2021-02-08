6b3bcea810f2824e3abc44778ce04aa89089cd55
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2021-02-08 17:16:40 +0100

    comment of use

--- crates-packages-modules/restaurant/src/lib.rs
+++ crates-packages-modules/restaurant/src/lib.rs
@@ -5,6 +5,7 @@ pub fn eat_at_restaurant() {
     crate::front_of_house::hosting::add_to_waitlist();
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+    //Use path, see top of the file
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
