2fb1ea2c71ca909c7b88e62d36f9093719707070
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2021-02-11 23:06:29 +0100

    module and referencing

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,4 +1,14 @@
+mod referencing;
+
 fn main() {
-    let v: Vec<i32> = Vec::new();
+    let mut v: Vec<i32> = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
     let vi32 = vec![1, 2, 3];
+
+    referencing::vector_reference()
 }
+
