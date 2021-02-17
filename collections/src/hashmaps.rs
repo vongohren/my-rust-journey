04575746dafa6491e3e13f0b80948b52ff9ff451
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2021-02-17 23:24:46 +0100

    even more rust hasmapps, interessting word count solution

--- collections/src/hashmaps.rs
+++ collections/src/hashmaps.rs
@@ -37,6 +37,25 @@ pub fn main() {
 
     println!("{:?}", scores);
 
+    //Counting occurrences of words using a hash map that stores words and counts
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    //This code will print {"world": 2, "hello": 1, "wonderful": 1}. 
+    // The or_insert method actually returns a mutable reference (&mut V) to the value for this key. 
+    // Here we store that mutable reference in the count variable, so in order to assign to that value. 
+    // We must first dereference count using the asterisk (*). 
+    // The mutable reference goes out of scope at the end of the for loop. 
+    // So all of these changes are safe and allowed by the borrowing rules.
+
+    println!("{:?}", map);
+
 
     
 }
