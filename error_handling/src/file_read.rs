fc0a5b59b0a35e1cd5aaebffa199288811e14172
Snorre lothar von Gohren Edwin <snorre.edwin@gmail.com>
2021-03-17 22:47:52 +0100

    Feauter: error handlign, and a editorconfig file

---
+++ error_handling/src/file_read.rs
@@ -0,0 +1,30 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+pub fn main() {
+    // let f = File::open("hello.txt");
+
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
+
+    read_username_from_file();
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
---
+++ error_handling/src/main.rs
@@ -0,0 +1,11 @@
+mod file_read;
+
+fn main() {
+  // panic!("crash and burn");
+  // let v = vec![1, 2, 3];
+
+  // v[99];
+  
+  file_read::main();
+
+}
